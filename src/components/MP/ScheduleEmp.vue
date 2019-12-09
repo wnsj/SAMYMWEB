4b897cb125074a862bf38d697a3f04fa1993c194
wp214111 <wp214111@163.com>
2019-12-09 14:56:30 +0800

    排班管理修改样式

--- src/components/MP/ScheduleEmp.vue
+++ src/components/MP/ScheduleEmp.vue
@@ -19,7 +19,7 @@
 				<div class="col-xs-5 col-sm-5 col-md-5 col-lg-5" style="padding: 0; line-height: 34px;">
 					<p class="end-aline col-md-11 col-lg-11" style="padding-right:5px; padding-left:20px;">咨询师</p><span class="sign-left">:</span>
 				</div>
-				<div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+				<div class="col-xs-7 col-sm-7 col-md-7 col-lg-7">
           <emp ref="counselorChange" @employeeChange="counselorEmpChange"></emp>
 				</div>
 			</div>

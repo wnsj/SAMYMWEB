a8ebb50c1939fa48c11e46faaf42b866d0bf14b4
liaomupeng <521040317@qq.com>
2019-12-12 09:55:11 +0800

    购买和消费左侧跳转

--- src/components/MP/newtree.vue
+++ src/components/MP/newtree.vue
@@ -10,7 +10,8 @@
 				<dt v-bind:class="{bg1:num==0,bg2:num!=0}">
 					<i class="fa" @click="onShow(0)" v-bind:class="{'fa-minus-square':num==0,'fa-plus-square':num!=0}" aria-hidden="true"></i>
 					<span class="fa" @click="onShow(0)" v-bind:class="{'fa-minus-square':num==0,'fa-plus-square':num!=0}" aria-hidden="true"></span>
-					会员管理模块	
+					<b>会员管理模块</b>
+					<div style="clear:both"></div>	
 				</dt>
 				<dd>
 					<span class="fa" @click="onShow(0)" v-bind:class="{'fa-minus-square':num==0,'fa-plus-square':num!=0}" aria-hidden="true"></span>
@@ -105,11 +106,12 @@
 	#tree{width:200px;border:1px solid #ccc;}
 	#tree dl{ margin-bottom:0; height:50px;overflow:hidden; color:#795548;font-size:20px; background:#fff; }
 	#tree dl.noShow{height:auto; }
-	#tree dl dt{height:50px;text-align:center;background:#fff;}
+	#tree dl dt{height:50px;background:#fff;}
+	#tree dl dt i{display:block;width:18px;height:50px;text-align:left;line-height:50px; float:left;}
+	#tree dl dt span{display:block;width:18px;height:50px;text-align:left;line-height:50px; float:left;}
 	#tree dl dd{height:50px;text-align:center;}
 	#tree dl dd:hover{background:#f5f5f5;color:#603b2c;}
-	#tree dl i{ position:relative; display:inline-block;width:200px;height:50px;text-align:left;line-height:50px;text-indent:25px;}
-	#tree dl i:before{position:absolute;left:-25px; display:block;}
+	
 	.bg1{background:url(../../assets/img/sub_bg1.jpg)}
 	.bg2{background:url(../../assets/img/sub_bg2.jpg)}
 	.bg3{background:url(../../assets/img/sub_bg3.jpg)}

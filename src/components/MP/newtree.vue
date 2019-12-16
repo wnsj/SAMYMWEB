7830adc66776670038f3fc176c19e60b84f396d9
liaomupeng <521040317@qq.com>
2019-12-16 10:25:33 +0800

    树形菜单插件版

--- src/components/MP/newtree.vue
+++ src/components/MP/newtree.vue
@@ -5,115 +5,123 @@
 		<div class="col-md-12 col-lg-12 main-title">
 			<h1 class="titleCss">权限</h1>
 		</div>
-		<div id="tree">
-			<dl v-bind:class="{noShow:bool1}">
-			</dl>
-			<dl v-for="(item , index) in moduleList" :key='index' v-bind:class="{noShow:bool2}">
-				<dt v-bind:class="{bg5:bool2,bg8:!bool2}">
-					<i class="fa" @click="onShow(2)" v-bind:class="{'fa-minus-square':bool2,'fa-plus-square':!bool2}" aria-hidden="true"></i>
-					<span>
-						<input type="checkbox" id="checkbox4"/>
-						<label for="checkbox4" class="fa fa-check-square-o" aria-hidden="true"></label>
-            			<label for="checkbox4" class="fa fa-square-o" aria-hidden="true"></label>
-					</span>
-					<b>{{item.name}}</b>
-				</dt>
-				<!-- <dd class="bg4" v-for="(item1,index1) in item.subMList" :key='index'>
-					<span>
-						<input type="checkbox" id="checkbox5"/>
-						<label for="checkbox5" class="fa fa-check-square-o" aria-hidden="true"></label>
-            			<label for="checkbox5" class="fa fa-square-o" aria-hidden="true"></label>
-					</span>
-					<b>{{item1.name}}</b>
-				</dd> -->
-			</dl>
-			<!-- <dl v-bind:class="{noShow:bool3}">
-				<dt v-bind:class="{bg5:bool3,bg7:!bool3}">
-					<i class="fa" @click="onShow(3)" v-bind:class="{'fa-minus-square':bool3,'fa-plus-square':!bool3}" aria-hidden="true"></i>
-					<span>
-						<input type="checkbox" id="checkbox6"/>
-						<label for="checkbox6" class="fa fa-check-square-o" aria-hidden="true"></label>
-            			<label for="checkbox6" class="fa fa-square-o" aria-hidden="true"></label>
-					</span>
-					<b>基础设备</b>
-				</dt>
-				<dd class="bg3">
-					<span>
-						<input type="checkbox" id="checkbox7"/>
-						<label for="checkbox7" class="fa fa-check-square-o" aria-hidden="true"></label>
-            			<label for="checkbox7" class="fa fa-square-o" aria-hidden="true"></label>
-					</span>
-					<b>消费管理</b>
-				</dd>
-				<dd class="bg4">
-					<span>
-						<input type="checkbox" id="checkbox8"/>
-						<label for="checkbox8" class="fa fa-check-square-o" aria-hidden="true"></label>
-            			<label for="checkbox8" class="fa fa-square-o" aria-hidden="true"></label>
-					</span>
-					<b>消费管理</b>
-				</dd>
-			</dl> -->
-		</div>
+		<v-tree :data="treeData" checkable multiple @select="selectFn" @check="checkFn" show-line></v-tree>
 	</div>
 
 </template>
 
 
 <script>
+	import Vue from 'vue'
+    //import css
+    import 'vue-beauty/package/style/vue-beauty.min.css'
+    //import components
+    import vueBeauty from 'vue-beauty'
+    Vue.use(vueBeauty)
+
+    //OR
+    import {alert} from 'vue-beauty'
 	export default {
 		components: {
 		
 		},
 		data() {
 			return {
-				moduleList:[
+				treeData: [
 					{
-						name:"目录1",
-						subMList:[
+						title: '目录1',
+						expanded: true,
+						selected: true,
+						children: [
 							{
-								name:'目录1——1'
+								title: '目录1——1',
+								expanded: true,
+								children: [{
+									title: 'leaf',
+									disableCheckbox: true
+								}, {
+									title: 'leaf',
+								}]
+							}, 
+							{
+								title: '目录1——2',
+								checked: true,
+								children: [{
+									title: "<span style='color: #08c'>sss</span>"
+								}]
 							},
 							{
-								name:'目录1——2'
-							},
-							{
-								name:'目录1——3'
+								title: '目录1——3',
+								checked: true,
+								children: [{
+									title: "<span style='color: #08c'>sss</span>"
+								}]
 							}
-						],
+						]
 					},
 					{
-						name:"目录1",
-						subMList:[
+						title: '目录1',
+						expanded: true,
+						selected: true,
+						children: [
 							{
-								name:'目录1——1'
+								title: '目录1——1',
+								expanded: true,
+								children: [{
+									title: 'leaf',
+									disableCheckbox: true
+								}, {
+									title: 'leaf',
+								}]
+							}, 
+							{
+								title: '目录1——2',
+								checked: true,
+								children: [{
+									title: "<span style='color: #08c'>sss</span>"
+								}]
 							},
 							{
-								name:'目录1——2'
-							},
-							{
-								name:'目录1——3'
+								title: '目录1——3',
+								checked: true,
+								children: [{
+									title: "<span style='color: #08c'>sss</span>"
+								}]
 							}
-						],
+						]
 					},
 					{
-						name:"目录1",
-						subMList:[
+						title: '目录1',
+						expanded: true,
+						selected: true,
+						children: [
 							{
-								name:'目录1——1'
+								title: '目录1——1',
+								expanded: true,
+								children: [{
+									title: 'leaf',
+									disableCheckbox: true
+								}, {
+									title: 'leaf',
+								}]
+							}, 
+							{
+								title: '目录1——2',
+								checked: true,
+								children: [{
+									title: "<span style='color: #08c'>sss</span>"
+								}]
 							},
 							{
-								name:'目录1——2'
-							},
-							{
-								name:'目录1——3'
+								title: '目录1——3',
+								checked: true,
+								children: [{
+									title: "<span style='color: #08c'>sss</span>"
+								}]
 							}
-						],
+						]
 					}
-				],
-				bool1:false,
-				bool2:false,
-				bool3:false
+				]
 			};
 		},
 		methods: {
@@ -141,26 +149,7 @@
 </script>
 
 <style>
-	#tree{width:200px; margin-top:50px;}
-	#tree dl{ margin-bottom:0; height:50px;overflow:hidden; color:#795548;font-size:20px; background:#fff; }
-	#tree dl.noShow{height:auto; }
-	#tree dl dt{height:50px;overflow:hidden;}
-	#tree dl dt i{display:block;width:18px;height:50px;text-align:left;line-height:50px; float:left; margin-right:8px;overflow:hidden;}
-	#tree dl span{position:relative;display:block;width:16px;height:16px;padding:17px 0;float:left;margin-right:8px;}
-	#tree dl span input{position:absolute; left:0;top:17px; margin-top:0;opacity:0; z-index:9;}
-	#tree dl span label{position:absolute; left:0;top:16px;}
-	#tree dl span label.fa-check-square-o{display:none;}
-	#tree dl span input:checked + label.fa-check-square-o{display:block;}
-	#tree dl dt b{display:block;width:130px;height:50px;text-align:left;line-height:50px; float:left;}
-	#tree dl dd{height:50px;text-align:left; text-indent:26px; overflow:hidden;}
-	#tree dl dd b{display:block;width:150px;height:50px;text-align:left;line-height:50px; float:left;font-weight:normal}
-	
-	.bg1{background:url(../../assets/img/sub_bg1.jpg)}
-	.bg2{background:url(../../assets/img/sub_bg2.jpg)}
-	.bg3{background:url(../../assets/img/sub_bg3.jpg)}
-	.bg4{background:url(../../assets/img/sub_bg4.jpg)}
-	.bg5{background:url(../../assets/img/sub_bg5.jpg)}
-	.bg6{background:url(../../assets/img/sub_bg6.jpg)}
-	.bg7{background:url(../../assets/img/sub_bg7.jpg)}
-	.bg8{background:url(../../assets/img/sub_bg8.jpg)}
+	.ant-tree{
+		text-align:left;
+	}
 </style>

3f8c96e0952403e62355411f63a1ac443088f320
muwenliang <a56299186@163.com>
2019-12-18 17:24:09 +0800

    修改预约样式

--- src/components/MP/Order.vue
+++ src/components/MP/Order.vue
@@ -43,6 +43,18 @@
 					</select>
 				</div>
 			</div>
+			<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+				<div class="col-xs-5 col-sm-5 col-md-5 col-lg-5  text-right" style="padding: 0; line-height: 34px;">
+					<p class="end-aline col-md-11 col-lg-11" style="padding-right:5px; padding-left:20px;">是否取消</p><span class="sign-left">:</span>
+				</div>
+				<div class="col-xs-7 col-sm-7 col-md-7 col-lg-7">
+					<select class="form-control" v-model="state">
+						<option value="">--未选择--</option>
+						<option value="1">未取消</option>
+						<option value="0">已取消</option>
+					</select>
+				</div>
+			</div>
 		</div>
 		<div class="row" style="margin-top: 15px;">
 			<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6" style="padding-left:0.8%;">
@@ -75,18 +87,7 @@
 			</div>
 		</div>
 		<div class="row" style="margin-top: 15px;padding-bottom:1.5%;">
-			<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-				<div class="col-xs-5 col-sm-5 col-md-5 col-lg-5  text-right" style="padding: 0; line-height: 34px;">
-					<p class="end-aline col-md-11 col-lg-11" style="padding-right:5px; padding-left:20px;">是否取消</p><span class="sign-left">:</span>
-				</div>
-				<div class="col-xs-7 col-sm-7 col-md-7 col-lg-7">
-					<select class="form-control" v-model="state">
-						<option value="">--未选择--</option>
-						<option value="1">未取消</option>
-						<option value="0">已取消</option>
-					</select>
-				</div>
-			</div>
+			
 			<button type="button" class="btn btn-warning pull-right m_r_10" style="margin-right:2.5%;" data-toggle="modal"
 			 v-on:click="addOrder()">添加预约</button>
 			<button type="button" class="btn btn-primary pull-right m_r_10" style="margin-right:1.5%;" data-toggle="modal"

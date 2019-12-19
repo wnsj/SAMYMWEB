602fd733dffbaee52a614b8ac77717a71ebe1db7
muwenliang <a56299186@163.com>
2019-12-19 10:16:18 +0800

    --

--- src/components/common/Paging.vue
+++ src/components/common/Paging.vue
@@ -66,9 +66,16 @@
         this.$emit('change',  this.cur)
       },
       setParam: function(all,cur,dataNum) {
-        this.all=parseInt(all)
-        this.cur=parseInt(cur)
-        //this.num=parseInt(num)
+				if(this.isBlank(all)){
+					this.all='0'
+				}else{
+					this.all=parseInt(all)
+				}
+        if(this.isBlank(cur)){
+        	this.cur='0'
+        }else{
+        	this.cur=parseInt(cur)
+        }
         this.dataNum=parseInt(dataNum)
       },
     },

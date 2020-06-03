e9ce548030206f839e75b2238f712c345fa8399b
wusong88 <1642168383@qq.com>
2020-06-03 15:12:43 +0800

    20200603ws_style

--- src/components/MP/Visitor.vue
+++ src/components/MP/Visitor.vue
@@ -171,10 +171,12 @@
 						</thead>
 						<tbody>
 							<tr v-for="(item,index) in visitorList" :key="index" v-on:dblclick="selectRule('3',item)">
-								<td class="text-center" v-has="'SAMY:MP:Visitor:Update'">
+								<td class="text-center" v-has="'SAMY:MP:Visitor:Update'" style="position: relative;" >
 									<!-- <button type="button" class="btn btn-warning" v-on:click="editorAction(item)">编辑
                                 </button> -->
-									<input type="radio" style="border-radius:5px;" name="复选框" :value="index" v-model="checkedValue" />
+									<input :id="'edit'+(index+1)" class="editradio" type="radio" style="border-radius:5px;" name="复选框" :value="index" v-model="checkedValue" />
+                                    <label :for="'edit'+(index+1)" class="editlabel" ></label>
+
 								</td>
 								<td class="text-center">{{item.visId}}</td>
 								<td class="text-center">{{item.visitorName}}</td>
@@ -572,6 +574,43 @@
 </script>
 
 <style>
+    input.editradio[type='radio']{
+       width: 20px;
+       height: 20px;
+      opacity: 0;
+    }
+    label.editlabel {
+      position: absolute;
+      left: 28px;
+      bottom: 12px;
+      width: 20px;
+      height: 20px;
+      border-radius: 50%;
+      border: 1px solid #999;
+      z-index: 99;
+    }
+
+    /*设置选中的input的样式*/
+    /* + 是兄弟选择器,获取选中后的label元素*/
+    input.editradio:checked+label.editlabel {
+      background-color: #3c86f1;
+      border: 1px solid #3c86f1;
+    }
+
+    input.editradio:checked+label.editlabel::after {
+      position: absolute;
+      content: '';
+      width: 5px;
+      height: 10px;
+      top: 3px;
+      left: 6px;
+      border: 2px solid #fff;
+      border-top: none;
+      border-left: none;
+      transform: rotate(45deg);
+    }
+
+
 	/*分页需要的样式*/
 	.page {
 		width: 100%;
@@ -605,4 +644,6 @@
 			display: none
 		}
 	}
+
+
 </style>

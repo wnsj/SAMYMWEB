4ea7b47b85999e56800349ec4b72b3473900c272
muwenliang <a56299186@163.com>
2020-07-02 09:35:49 +0800

    修改收入表

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,9 +4,9 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-    API_HOST:'"https://cs.miyuejk.com:4093/samy"',
+    // API_HOST:'"https://cs.miyuejk.com:4093/samy"',
     // API_HOST:'"http://172.16.56.1:8080"',
-	//API_HOST:'"http://172.16.3.58:8080"',
+	API_HOST:'"http://172.16.3.58:8080"',
 	// API_HOST:'"http://172.16.3.33:8082/"'
 	// API_HOST:'"http://172.16.3.33:8082/"'
 })

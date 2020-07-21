6f117ee0ddd458aa6bcdf3e01a5aca3fab004120
sunyl-5664 <lx5664@hotmail.com>
2020-07-21 13:46:50 +0800

    所有可以列合计的均要列式合计。增加助理列及查询窗口

--- src/components/MP/Refund.vue
+++ src/components/MP/Refund.vue
@@ -6,33 +6,63 @@
             <h1 class="titleCss">退费明细</h1>
         </div>
         <div class="row newRow">
-			<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3" v-if="accountType==true">
-				<div class="col-md-5 col-lg-5 text-right nopad">
-					<p class="end-aline col-md-11 col-lg-11">门店</p><span class="sign-left">:</span>
-				</div>
-				<div class="col-md-7 col-lg-7">
-					<store ref='store' @storeChange='storeChange'></store>
-				</div>
-			</div>
-            <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-            	<div class="col-md-5 col-lg-5 text-right nopad">
-            		<p class="end-aline col-md-11 col-lg-11">姓名</p><span class="sign-left">:</span>
-            	</div>
-            	<div class="col-md-7 col-lg-7"><input class="form-control" type="text" value="" v-model="visitorName">
-            	</div>
+            <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3" v-if="accountType==true">
+                <div class="col-md-5 col-lg-5 text-right nopad">
+                    <p class="end-aline col-md-11 col-lg-11">门店</p><span class="sign-left">:</span>
+                </div>
+                <div class="col-md-7 col-lg-7">
+                    <store ref='store' @storeChange='storeChange'></store>
+                </div>
             </div>
-           <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-           	<div class="col-md-3 col-lg-3 text-right nopad">
-           		<p class="end-aline col-md-11 col-lg-11">日期范围</p><span class="sign-left">:</span>
-           	</div>
-           	<div class="col-md-9 col-lg-9">
-           		<dPicker class="wd100" v-model="dateArr" type="format" format="YYYY-MM-DD" range>
-					<template v-slot:header="{ emit }">
-						<div class="text-left">	</div>
-					</template>
-				</dPicker>
-           	</div>
-           </div>
+            <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                <div class="col-md-5 col-lg-5 text-right nopad">
+                    <p class="end-aline col-md-11 col-lg-11">姓名</p><span class="sign-left">:</span>
+                </div>
+                <div class="col-md-7 col-lg-7"><input class="form-control" type="text" value="" v-model="visitorName">
+                </div>
+            </div>
+            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                <div class="col-md-3 col-lg-3 text-right nopad">
+                    <p class="end-aline col-md-11 col-lg-11">日期范围</p><span class="sign-left">:</span>
+                </div>
+                <div class="col-md-9 col-lg-9">
+                    <dPicker class="wd100" v-model="dateArr" type="format" format="YYYY-MM-DD" range>
+                        <template v-slot:header="{ emit }">
+                            <div class="text-left"></div>
+                        </template>
+                    </dPicker>
+                </div>
+            </div>
+        </div>
+        <div class="row newRow">
+            <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                <div class="col-md-5 col-lg-5 text-right jh-ad-1">
+                    <p class="end-aline col-md-11 col-lg-11 jh-pa-1">咨询师</p><span
+                    class="sign-left">:</span>
+                </div>
+                <div class="col-md-7 col-lg-7">
+                    <select class="form-control" v-model="couId">
+                        <option value="">--未选择--</option>
+                        <option v-for="(item,index) in couList" :key="index" v-bind:value="item.empId">
+                            {{item.empName}}
+                        </option>
+                    </select>
+                </div>
+            </div>
+            <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                <div class="col-md-5 col-lg-5 text-right jh-ad-1">
+                    <p class="end-aline col-md-11 col-lg-11 jh-pa-1">咨询顾问</p><span
+                    class="sign-left">:</span>
+                </div>
+                <div class="col-md-7 col-lg-7">
+                    <select class="form-control" v-model="conId">
+                        <option value="">--未选择--</option>
+                        <option v-for="(item,index) in empList" :key="index" v-bind:value="item.empId">
+                            {{item.empName}}
+                        </option>
+                    </select>
+                </div>
+            </div>
             <button type="button" class="btn btn-primary pull-right m_r_10 jh-mr-30"
                     data-toggle="modal"
                     v-on:click="queryObjectList(1)">查询
@@ -48,10 +78,11 @@
                             <th class="text-center">门店名称</th>
                             <th class="text-center">退款人</th>
                             <th class="text-center">咨询师</th>
+                            <th class="text-center">咨询助理</th>
                             <th class="text-center">产品</th>
                             <th class="text-center">退费课时</th>
-							<th class="text-center">退费金额</th>
-							<th class="text-center">时间</th>
+                            <th class="text-center">退费金额</th>
+                            <th class="text-center">时间</th>
                         </tr>
                         </thead>
                         <tbody>
@@ -59,8 +90,9 @@
                             <td class="text-center">{{item.storeName}}</td>
                             <td class="text-center">{{item.visitorName}}</td>
                             <td class="text-center">{{item.proEmpName}}</td>
-							<td class="text-center">{{item.proName}}</td>
-							<td class="text-center">{{item.refCount}}</td>
+                            <td class="text-center">{{item.empName}}</td>
+                            <td class="text-center">{{item.proName}}</td>
+                            <td class="text-center">{{item.refCount}}</td>
                             <td class="text-center">{{item.realRefund}}</td>
                             <td class="text-center">{{item.createDate | dateFormatFilter('YYYY-MM-DD')}}</td>
                         </tr>
@@ -83,9 +115,11 @@
 <script>
 
 
-	import dPicker from 'vue2-datepicker'
-	import store from '../common/Store.vue'
+    import dPicker from 'vue2-datepicker'
+    import store from '../common/Store.vue'
     import Paging from '../common/paging'
+    import cou from '../common/Employee.vue' //咨询师
+    import con from '../common/Employee.vue'	//咨询顾问
     import {
         init
     } from '@/../static/js/common.js'
@@ -93,19 +127,26 @@
     export default {
         components: {
             Paging,
-			dPicker,
-			store,
+            dPicker,
+            store,
+            con,
+            cou
         },
         data() {
             return {
                 objList: [],
                 fixedHeader: false,
-				visitorName:'',
-				dateArr:'',
-				begDate:'',
-				endDate:'',
-				storeId: this.storeId(),
-				accountType:this.accountType(),
+                visitorName: '',
+                dateArr: '',
+                begDate: '',
+                endDate: '',
+                couId: '',
+                conId: '',
+                storeId: this.storeId(),
+                accountType: this.accountType(),
+
+                empList: [],
+                couList: [],
 
                 //分页需要的数据
                 pages: '', //总页数
@@ -122,21 +163,59 @@
             },
 
             storeChange(param) {
-            	if (this.isBlank(param)) {
-            		this.storeId = ""
-            	} else {
-            		this.storeId = param.storeId
-            	}
+                if (this.isBlank(param)) {
+                    this.storeId = ""
+                } else {
+                    this.storeId = param.storeId
+                }
+            },
+            getCou() {
+                var url = this.url + '/employeeAction/getAllEmpByPosName'
+                this.$ajax({
+                    method: 'POST',
+                    url: url,
+                    headers: {
+                        'Content-Type': this.contentType,
+                        'Access-Token': this.accessToken
+                    },
+                    data: {
+                        posName: "咨询师"
+                    },
+                    dataType: 'json',
+                }).then(res => {
+                    this.couList = res.data.retData
+                }).catch(error => {
+                    console.log(error);
+                })
+            },
+            getEmp() {
+                var url = this.url + '/employeeAction/getAllEmpByPosName'
+                this.$ajax({
+                    method: 'POST',
+                    url: url,
+                    headers: {
+                        'Content-Type': this.contentType,
+                        'Access-Token': this.accessToken
+                    },
+                    data: {
+                        posName: "咨询顾问"
+                    },
+                    dataType: 'json',
+                }).then(res => {
+                    this.empList = res.data.retData
+                }).catch(error => {
+                    console.log(error);
+                })
             },
             //check the list of store
             queryObjectList(page) {
-				if(this.dateArr.length > 0 && !this.isBlank(this.dateArr[0]) && !this.isBlank(this.dateArr[1]))	{
-					this.begDate = this.moment(this.dateArr[0],'YYYY-MM-DD 00:00:00')
-					this.endDate = this.moment(this.dateArr[1],'YYYY-MM-DD 23:59:59')
-				}else{
-					this.begDate=''
-					this.endDate=''
-				}
+                if (this.dateArr.length > 0 && !this.isBlank(this.dateArr[0]) && !this.isBlank(this.dateArr[1])) {
+                    this.begDate = this.moment(this.dateArr[0], 'YYYY-MM-DD 00:00:00')
+                    this.endDate = this.moment(this.dateArr[1], 'YYYY-MM-DD 23:59:59')
+                } else {
+                    this.begDate = ''
+                    this.endDate = ''
+                }
                 var url = this.url + '/refundAction/queryRefund'
                 this.$ajax({
                     method: 'POST',
@@ -146,10 +225,12 @@
                         'Access-Token': this.accessToken
                     },
                     data: {
-						storeId: this.storeId,
-						visitorName:this.visitorName,
-						begDate:this.begDate,
-						endDate:this.endDate,
+                        storeId: this.storeId,
+                        counselor: this.couId,
+                        empId: this.conId,
+                        visitorName: this.visitorName,
+                        begDate: this.begDate,
+                        endDate: this.endDate,
 
                         page: page.toString(),
                         pageSize: this.pageSize
@@ -196,6 +277,8 @@
             }
         },
         mounted() {
+            this.getCou()
+            this.getEmp()
             window.addEventListener('scroll', this.handleScroll, true);
             init();
         },

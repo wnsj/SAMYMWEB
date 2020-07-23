f1d3a56b1d8acb7741099563634d473e42fa9554
sunyl-5664 <lx5664@hotmail.com>
2020-07-23 14:31:18 +0800

    收入情况表 点击查询查询条件隐藏

--- src/components/MP/Income.vue
+++ src/components/MP/Income.vue
@@ -5,124 +5,129 @@
         </div>
         <div class="top">
             <el-form label-position="right" label-width="100px" :inline="true" size="small" :model="param">
-                <el-row style="margin-top: 2%">
-                    <el-col :span="6">
-                        <el-form-item label="访问类型:">
-                            <el-select v-model="param.visitType" clearable placeholder="请选择"
-                                       @change="getObj(param.visitType)">
-                                <el-option v-for="item in list"
-                                           :key="item.id"
-                                           :label="item.name"
-                                           :value="item.id">
-                                </el-option>
-                            </el-select>
-                        </el-form-item>
-                    </el-col>
-                    <el-col :span="6">
-                        <el-form-item label="咨客判定:">
-                            <el-select v-model="param.visitState" clearable placeholder="请选择">
-                                <el-option v-for="item in vsJugList"
-                                           :key="item.vsId"
-                                           :label="item.vsName"
-                                           :value="item.vsId">
-                                </el-option>
-                            </el-select>
-                        </el-form-item>
-                    </el-col>
-                    <el-col :span="6" style="margin-left: 3%">
-                        <el-form-item label="续流状态:">
-                            <el-select v-model="param.continState" clearable placeholder="请选择">
-                                <el-option v-for="item in continueList"
-                                           :key="item.vsId"
-                                           :label="item.vsName"
-                                           :value="item.vsId">
-                                </el-option>
-                            </el-select>
-                        </el-form-item>
-                    </el-col>
-                </el-row>
+                <el-collapse-transition>
+                    <div class="searchForm" v-show="showSelect">
+                        <el-row style="margin-top: 1%">
+                            <el-col :span="6">
+                                <el-form-item label="访问类型:">
+                                    <el-select v-model="param.visitType" clearable placeholder="请选择"
+                                               @change="getObj(param.visitType)">
+                                        <el-option v-for="item in list"
+                                                   :key="item.id"
+                                                   :label="item.name"
+                                                   :value="item.id">
+                                        </el-option>
+                                    </el-select>
+                                </el-form-item>
+                            </el-col>
+                            <el-col :span="6">
+                                <el-form-item label="咨客判定:">
+                                    <el-select v-model="param.visitState" clearable placeholder="请选择">
+                                        <el-option v-for="item in vsJugList"
+                                                   :key="item.vsId"
+                                                   :label="item.vsName"
+                                                   :value="item.vsId">
+                                        </el-option>
+                                    </el-select>
+                                </el-form-item>
+                            </el-col>
+                            <el-col :span="6" style="margin-left: 3%">
+                                <el-form-item label="续流状态:">
+                                    <el-select v-model="param.continState" clearable placeholder="请选择">
+                                        <el-option v-for="item in continueList"
+                                                   :key="item.vsId"
+                                                   :label="item.vsName"
+                                                   :value="item.vsId">
+                                        </el-option>
+                                    </el-select>
+                                </el-form-item>
+                            </el-col>
+                        </el-row>
+                        <el-row>
+                            <el-col :span="6">
+                                <el-form-item label="门店:" v-if="accountType == true">
+                                    <el-select v-model="param.storeId" filterable clearable placeholder="请选择">
+                                        <el-option v-for="item in storeList"
+                                                   :key="item.storeId"
+                                                   :label="item.storeName"
+                                                   :value="item.storeId">
+                                        </el-option>
+                                    </el-select>
+                                </el-form-item>
+                            </el-col>
+                            <el-col :span="6">
+                                <el-form-item label='咨客姓名:'>
+                                    <el-input v-model="param.vsName" placeholder="咨客姓名" clearable></el-input>
+                                </el-form-item>
+                            </el-col>
+                            <el-col :span="6" style="margin-left: 3%">
+                                <el-form-item label="是否全款:">
+                                    <el-select v-model="param.isArrears" filterable clearable placeholder="请选择">
+                                        <el-option v-for="item in isArrearsList"
+                                                   :key="item.id"
+                                                   :label="item.name"
+                                                   :value="item.id">
+                                        </el-option>
+                                    </el-select>
+                                </el-form-item>
+                            </el-col>
 
-                <el-row>
-                    <el-col :span="6">
-                        <el-form-item label="门店:" v-if="accountType == true">
-                            <el-select v-model="param.storeId" filterable clearable placeholder="请选择">
-                                <el-option v-for="item in storeList"
-                                           :key="item.storeId"
-                                           :label="item.storeName"
-                                           :value="item.storeId">
-                                </el-option>
-                            </el-select>
-                        </el-form-item>
-                    </el-col>
-                    <el-col :span="6">
-                        <el-form-item label='咨客姓名:'>
-                            <el-input v-model="param.vsName" placeholder="咨客姓名" clearable></el-input>
-                        </el-form-item>
-                    </el-col>
-                    <el-col :span="6" style="margin-left: 3%">
-                        <el-form-item label="是否全款:">
-                            <el-select v-model="param.isArrears" filterable clearable placeholder="请选择">
-                                <el-option v-for="item in isArrearsList"
-                                           :key="item.id"
-                                           :label="item.name"
-                                           :value="item.id">
-                                </el-option>
-                            </el-select>
-                        </el-form-item>
-                    </el-col>
+                        </el-row>
+                        <el-row>
+                            <el-col :span="6">
+                                <el-form-item label="咨询师:">
+                                    <el-select v-model="param.couId" filterable clearable placeholder="请选择">
+                                        <el-option v-for="item in couList"
+                                                   :key="item.empId"
+                                                   :label="item.empName"
+                                                   :value="item.empId">
+                                        </el-option>
+                                    </el-select>
+                                </el-form-item>
+                            </el-col>
+                            <el-col :span="6">
+                                <el-form-item label="咨询顾问:">
+                                    <el-select v-model="param.empId" filterable clearable placeholder="请选择">
+                                        <el-option v-for="item in empList"
+                                                   :key="item.empId"
+                                                   :label="item.empName"
+                                                   :value="item.empId">
+                                        </el-option>
+                                    </el-select>
+                                </el-form-item>
+                            </el-col>
+                            <el-col :span="11">
+                                <el-form-item label="购买时间:">
+                                    <el-date-picker
+                                        v-model="param.startTime"
+                                        :picker-options="pickerOptions0"
+                                        type="date"
+                                        placeholder="开始时间">
+                                    </el-date-picker>
+                                    <span> - </span>
+                                    <el-date-picker
+                                        v-model="param.endTime"
+                                        :picker-options="pickerOptions1"
+                                        type="date"
+                                        placeholder="结束时间">
+                                    </el-date-picker>
+                                </el-form-item>
+                            </el-col>
+                        </el-row>
+                        <el-row>
 
-                </el-row>
-                <el-row>
-                    <el-col :span="6">
-                        <el-form-item label="咨询师:">
-                            <el-select v-model="param.couId" filterable clearable placeholder="请选择">
-                                <el-option v-for="item in couList"
-                                           :key="item.empId"
-                                           :label="item.empName"
-                                           :value="item.empId">
-                                </el-option>
-                            </el-select>
-                        </el-form-item>
-                    </el-col>
-                    <el-col :span="6">
-                        <el-form-item label="咨询顾问:">
-                            <el-select v-model="param.empId" filterable clearable placeholder="请选择">
-                                <el-option v-for="item in empList"
-                                           :key="item.empId"
-                                           :label="item.empName"
-                                           :value="item.empId">
-                                </el-option>
-                            </el-select>
-                        </el-form-item>
-                    </el-col>
-                    <el-col :span="11">
-                        <el-form-item label="购买时间:">
-                            <el-date-picker
-                                v-model="param.startTime"
-                                :picker-options="pickerOptions0"
-                                type="date"
-                                placeholder="开始时间">
-                            </el-date-picker>
-                            <span> - </span>
-                            <el-date-picker
-                                v-model="param.endTime"
-                                :picker-options="pickerOptions1"
-                                type="date"
-                                placeholder="结束时间">
-                            </el-date-picker>
-                        </el-form-item>
-                    </el-col>
-                </el-row>
-                <el-row>
-
-                    <el-col :push="8">
-                        <el-button type="primary" size="small"
-                                   style="width: 85px"
-                                   @click="getWaterByCondition">查询
-                        </el-button>
-                    </el-col>
-
-                </el-row>
+                            <el-col :push="8">
+                                <el-button type="primary" size="small"
+                                           style="width: 85px"
+                                           @click="getWater">查询
+                                </el-button>
+                            </el-col>
+                        </el-row>
+                    </div>
+                </el-collapse-transition>
+                <div class="arrow-bottom" @click="showSelect = !showSelect">
+                    <div style="position: absolute; margin-left: -6px;color: black" class="el-icon-arrow-down"></div>
+                </div>
             </el-form>
         </div>
 
@@ -266,6 +271,7 @@
                     continState: '',
                     isArrears: ''
                 },
+                showSelect: true,
                 list: [{id: 1, name: "初访"}, {id: 2, name: "复访"}],
                 isArrearsList: [{id: 1, name: "全款"}, {id: 0, name: "欠费"}],
                 storeList: [],
@@ -433,6 +439,10 @@
             },
 
             // 获取初访咨询方向汇总数据
+            getWater() {
+                this.showSelect = false
+                this.getWaterByCondition()
+            },
             async getWaterByCondition() {
                 var url = this.url + '/consumAction/getWaterByCondition'
                 this.$ajax({
@@ -480,6 +490,24 @@
     }
 </script>
 
-<style scoped>
-
+<style>
+    .searchForm {
+        background-color: white;
+    / / margin-top: 40 px;
+        padding-top: 30px;
+        border-radius: 5px;
+        padding-bottom: 22px;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04)
+    }
+    .arrow-bottom{
+        content: "";
+        display: block;
+        border-width: 0px 15px 15px;
+        border-style: none solid solid;
+        border-color: transparent transparent #e3e3e3;
+        -webkit-transform: rotate(-180deg);
+        margin-left: 45%;
+        z-index: 9999;
+        width: 100px;
+    }
 </style>

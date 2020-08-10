abac9ee6662f84ddcf92d585a9686e3d8fb11de3
wusong88 <1642168383@qq.com>
2020-08-10 10:26:41 +0800

    0810-ws2

--- src/components/MP/QueVsAnswer.vue
+++ src/components/MP/QueVsAnswer.vue
@@ -147,7 +147,7 @@
 		</div>
 
 		<div>
-			<el-table  :data="tableData" :cell-style="cellStyle" :height="tableHeight" @row-dblclick="toDetails" :header-cell-style="headerStyle"
+			<el-table  :data="tableData" :cell-style="cellStyle" :height="400" @row-dblclick="toDetails" :header-cell-style="headerStyle"
 			 style="width: 95%;margin: 20px auto 20px" :summary-method="getSummaries" show-summary border>
 				<el-table-column align="center" prop="vsName" label="咨客姓名" min-width="100">
 				</el-table-column>

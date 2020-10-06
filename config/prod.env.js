e54aced15f78db89df8bb29c53bf17f0a9cdf445
muwenliang <a56299186@163.com>
2020-10-06 15:27:01 +0800

    --

--- config/prod.env.js
+++ config/prod.env.js
@@ -4,5 +4,5 @@ module.exports = {
 	// API_HOST:'"https://cs.miyuejk.com:4093/samy"',
 	// API_HOST:'"https://172.16.213.210/samy"',
 	// API_HOST:'"http://172.16.213.210:8080/samy"',
-	API_HOST:'"http://vip.miyuexli.com:88/samy20200727"',
+	API_HOST:'"http://vip.miyuexli.com:88/samy20200930"',
 }

c5329691bff0b9e7e536a1599a464b03b14834cb
xiaoxiaodeguang <2724364263@qq.com>
2020-12-31 11:31:46 +0800

    一些小修改

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-	// API_HOST:'"https://cs.miyuejk.com:4093/samy"',
-	API_HOST:'"http://vip.miyuexli.com:88/samy20200930"',
+	API_HOST:'"https://cs.miyuejk.com:4093/samy"',
+	// API_HOST:'"http://vip.miyuexli.com:88/samy20200930"',
 }

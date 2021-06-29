3468866a50ac3d66aa9c2f1f82d518019c8abbf7
kristenmi <1970594608@qq.com>
2021-06-29 18:07:01 +0800

    修改新增信息页面
    Signed-off-by: kristenmi <1970594608@qq.com>

--- src/components/MP/SubOrder/AddInformation.vue
+++ src/components/MP/SubOrder/AddInformation.vue
@@ -81,90 +81,131 @@
                         </select>
                     </div>
                 </div>
-                <div class="col-md-6 form-group clearfix jh-wd-33">
+                <div class="col-md-6 form-group clearfix">
                     <b>*</b>
-                    <label class="col-md-3 control-label text-right nopad end-aline">初访时间</label><span
-                    class="sign-left">:</span>
-                    <div class="col-md-8 addflex">
-                        <input type="text" class="form-control" v-model="orderClick.phone" placeholder="">
-                    </div>  
-                    <span class="sign-left">K</span>              
+                    <label class="col-md-2 control-label text-right nopad end-aline" >初访时间</label><span
+                        class="sign-left">:</span>
+                    <div class="col-md-5">
+                        <dPicker v-model="timeStart" format="YYYY-MM-DD" class="wd100" v-on:change="firstVisit()"></dPicker>
+                    </div>
                 </div>
                 <!-- 问卷 单选 -->
                 <div class="col-md-12 clearfix jh-ad-0 choosebox ">
-                    <input style="display:none;"  type="radio" name="choose" value=""> 是否填写现场调研问卷 
-                　　　　 <input type="radio" name="choose" value="1" class="cho1">  是
-                　　　　 <input type="radio" name="choose" value="0"> 否 <br/>
-                    <input style="display:none;" type="radio" name="one" value=""> 是否有过自杀、自伤、攻击暴力冲动、幻视、幻听等类似情况
-                　　　　 <input type="radio" name="one" value="1" class="cho2">  是
-                　　　　 <input type="radio" name="one" value="0"> 否 <br/>
-                    <input style="display:none;"  type="radio" name="two" value=""> 是否有过抑郁、焦虑、暴躁、长期情绪不稳等类似情况
-                　　　　 <input type="radio" name="two" value="1" class="cho3">  是
-                　　　　 <input type="radio" name="two" value="0"> 否 <br/>
-                    <input style="display:none;"  type="radio" name="three" value=""> 是否有家族精神病史
-                　　　　 <input type="radio" name="three" value="1" class="cho4">  是
-                　　　　 <input type="radio" name="three" value="0"> 否 <br/>
-                    <input style="display:none;" type="radio" name="four" value=""> 是否有服用过精神药物或前往精神科/心理科就诊
-                　　　　 <input type="radio" name="four" value="1" class="cho5">  是
-                　　　　 <input type="radio" name="four" value="0"> 否 <br/>
-                    <textarea name="textarea" class="text" cols="105" rows="2" placeholder="备注：就诊机构和就诊时间、服用药品"></textarea><br/>
-                    <input style="display:none;" type="radio" name="five" value=""> 是否曾在其它心理咨询机构进行过心理咨询
-                　　　　 <input type="radio" name="five" value="1" class="cho6">  是
-                　　　　 <input type="radio" name="five" value="0"> 否 <br/>
-                        <textarea name="textarea" cols="105" rows="2" placeholder="备注：机构、时间、次数、效果等"></textarea><br/>
-                    <input style="display:none;" type="radio" name="six" value="">是否愿意接受满意度回访
-                　　　　 <input type="radio" name="six" value="1" class="cho7">  是
-                　　　　 <input type="radio" name="six" value="0"> 否 <br/>
+                    <div class="question"> 是否填写现场调研问卷 </div>
+                    <div class="radio cho1">
+                        <input type="radio" id="1" name="one">
+                        <label for="1"></label> 是
+                    </div>
+                    <div class="radio">
+                        <input type="radio" id="2" name="one">
+                        <label for="2"></label> 否
+                    </div><br/>
+                    <div class="question"> 是否有过自杀、自伤、攻击暴力冲动、幻视、幻听等类似情况 </div>
+                    <div class="radio cho2">
+                        <input type="radio" id="3" name="two">
+                        <label for="3"></label> 是
+                    </div>
+                    <div class="radio">
+                        <input type="radio" id="4" name="two">
+                        <label for="4"></label> 否
+                    </div><br/>
+                    <div class="question"> 是否有过抑郁、焦虑、暴躁、长期情绪不稳等类似情况 </div>
+                    <div class="radio cho3">
+                        <input type="radio" id="5" name="three">
+                        <label for="5"></label> 是
+                    </div>
+                    <div class="radio">
+                        <input type="radio" id="6" name="three">
+                        <label for="6"></label> 否
+                    </div><br/>
+                    <div class="question"> 是否有家族精神病史 </div>
+                    <div class="radio cho4">
+                        <input type="radio" id="7" name="four">
+                        <label for="7"></label> 是
+                    </div>
+                    <div class="radio">
+                        <input type="radio" id="8" name="four">
+                        <label for="8"></label> 否
+                    </div><br/>
+                    <div class="question"> 是否有服用过精神药物或前往精神科/心理科就诊 </div>
+                    <div class="radio cho5">
+                        <input type="radio" id="9" name="five">
+                        <label for="9"></label> 是
+                    </div>
+                    <div class="radio">
+                        <input type="radio" id="10" name="five">
+                        <label for="10"></label> 否
+                    </div><br/>
+                    <textarea class="form-control" placeholder="备注：就诊机构和就诊时间、服用药品" v-model="time"></textarea>
+                    <div class="question"> 是否曾在其它心理咨询机构进行过心理咨询 </div>
+                    <div class="radio cho6">
+                        <input type="radio" id="11" name="six">
+                        <label for="11"></label> 是
+                    </div>
+                    <div class="radio">
+                        <input type="radio" id="12" name="six">
+                        <label for="12"></label> 否
+                    </div><br/>
+                    <textarea class="form-control" placeholder="备注：机构、时间、次数、效果等" v-model="time"></textarea>
+                    <div class="question"> 是否愿意接受满意度回访 </div>
+                    <div class="radio cho7">
+                        <input type="radio" id="13" name="seven">
+                        <label for="13"></label> 是
+                    </div>
+                    <div class="radio">
+                        <input type="radio" id="14" name="seven">
+                        <label for="14"></label> 否
+                    </div><br/>
                     <!-- 多选 -->
-                    <div class="checkbox1">
-                        <label class="col-md-1 control-label text-right nopad end-aline">可回访时间</label><span
-                        class="sign-left">:</span>
-                        <div class="col-md-8 timeselect">
-                            <select multip class="form-control" v-model="orderClick.phone">
-                                <option choose value="">未选择（可多选）</option>
-                                <option value="1">9-12点</option>
-                                <option value="2">12-14点</option>
-                                <option value="3">14-18点</option>
-                                <option value="3">18-20点</option>
-                            </select>
-                        </div>                        
+                    <div>
+                        <span class="demonstration">可回访时间：</span>
+                        <el-select v-model="timeselect" multiple placeholder="请选择">
+                            <el-option
+                            v-for="item in select"
+                            :key="item.value"
+                            :label="item.label"
+                            :value="item.value">
+                            </el-option>
+                        </el-select>
                     </div>
                     <div class="checkbox1">
-                        <label class="col-md-2 control-label text-right nopad end-aline">是否添加售后客服微信</label><span
-                        class="sign-left">:</span>
-                        <div class="col-md-8 timeselect">
-                            <select  class="form-control" v-model="orderClick.channel">
-                                <option choose value="客服一">客服一</option>
-                                <option value="客服二">客服二</option>
-                            </select>
-                        </div>
-                        <input class="inputWechat" v-model="orderClick.channel">
+                        <span class="demonstration">是否添加售后客服微信：</span>
+                        <el-select v-model="value1" multiple placeholder="请选择">
+                            <el-option
+                            v-for="item in options"
+                            :key="item.value"
+                            :label="item.label"
+                            :value="item.value">
+                            </el-option>
+                        </el-select>
+                        <!-- <el-input class="inputWechat" disabled v-model="value1"></el-input> -->
+                        <input type="text" disabled class="inputWechat" v-model="value1">
                     </div>
                     <div class="col-md-12  clearfix jh-ad-0">
                         <span>疾病史【疾病名称、程度、身体健康状况】</span><br/>
-                        <textarea name="textarea" cols="105" rows="2" ></textarea><br/>
+                        <textarea class="form-control" v-model="time"></textarea>
                     </div>
                     <div class="col-md-12  clearfix jh-ad-0">
                         <span>过敏史</span><br/>
-                        <textarea name="textarea" cols="105" rows="2"></textarea><br/>
+                        <textarea class="form-control" v-model="time"></textarea><br/>
                     </div>
                 </div>
                 <!-- 课程详情 -->
                 <div v-for='(item,index) in itemcount' :key="index" 
-                class="col-md-12 clearfix jh-ad-0 choosebox1">
-                    <div class="top">
-                        <div>结课时间:&nbsp;&nbsp;&nbsp;
-                            <input v-model="time">
-                        </div>
-                        <p>
+                class="col-md-10 clearfix jh-ad-0 choosebox1">
+                    <div class="classtop">
+                       <div class="classtop">
+                            结课时间:&nbsp;
+                            <dPicker v-model="timeStart" format="YYYY-MM-DD" ></dPicker>
+                       </div>
+                        <p >
                             填写人:789{{reason}}
                         </p>
                     </div>
-                    <div id="div" class="top">
+                    <div  class="classtop reasons">
                         节课原因:
-                        <textarea name="textarea" cols="90" rows="2" ></textarea><br/>
+                        <textarea class="form-control reason" name="textarea" cols="80" rows="2" ></textarea><br/>
                     </div>
-                    <!-- <button>新增结课信息</button> -->
                     <button id="btn" type="button" :disabled="isDisable" class="btn btn-primary pull-right margin-right-15 station"
                             data-toggle="modal" v-on:click="addForm()">新增结课信息
                     </button>
@@ -181,9 +222,7 @@
         </div>
     </div>
 </template>
-<script src="src/assets/js/selectMultip.js"></script>
 <script>
-    import selectMultip from '../../../assets/js/selectMultip.js'
     import dPicker from 'vue2-datepicker'
     import emp from '../../common/Employee.vue'
     import CounseRoom from '../../common/CounseRoom.vue'
@@ -192,27 +231,46 @@ export default{
         dPicker,
         emp,
         CounseRoom,
-        selectMultip,
     },
     data() {
         return {
 			succ: false,
             isDisable: false,
             itemcount:1,
-            orderClick:{
+            orderClick:{ //自定义表单绑定数据
                 channel:'',
                 phone:'' ,
             },
-            reduce:[], //课程详情
-            time:'',
-            reason:'',
-
-        }
+            timeStart:'', //时间选择器
+            timeselect:[], // 回访时间
+            value1: [], //微信多选绑定值
+            time:'',  //结课时间
+            reason:'', //节课原因
+            options: [{   //微信绑定值
+            value: '客服一',
+            label: '客服一'
+            }, {
+            value: '客服二',
+            label: '客服二'
+            }],
+            select: [{   //回访时间绑定值
+            value: '1',
+            label: '9-12点'
+            }, {
+            value: '2',
+            label: '12-14点'
+            }, {
+            value: '3',
+            label: '14-18点'
+            }, {
+            value: '4',
+            label: '18-20点'
+            }
+            ],
+            
+            }
     },
     methods: {
-        multip:function(){
-            selectMultip();
-        },
         initData(flag,param) {
 			$('#addInformation').modal({backdrop: 'static', keyboard: false});
             console.log(param)
@@ -229,6 +287,10 @@ export default{
         // 关闭弹窗
         closeCurrentPage() {
             this.$emit('closeCurrentPage')
+        },
+        // 初访
+        firstVisit() {
+
         },
 
     }
